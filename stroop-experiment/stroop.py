2db9d8b3b2ac480d75748b17660cd60ddf148eb8
michellebach <147568130+michellebach@users.noreply.github.com>
2025-02-17 22:01:00 -0800

    Add files via upload

--- stroop-experiment/stroop.py
+++ stroop-experiment/stroop.py
@@ -10,10 +10,25 @@ win = visual.Window([800,600],color="gray", units='pix',checkTiming=False)
 placeholder = visual.Rect(win,width=180,height=80, fillColor="lightgray",lineColor="black", lineWidth=6,pos=[0,0])
 word_stim = visual.TextStim(win,text="", height=40, color="black",pos=[0,0])
 instruction = visual.TextStim(win,text="Press the first letter of the ink color", height=20, color="black",pos=[0,-200])
+#add fixation cross
+fixation_cross = visual.TextStim(win, text = "+", color = "black", height = 15)
+
 while True:
     cur_stim = random.choice(stimuli)
     word_stim.setText(cur_stim)
     word_stim.setColor(cur_stim)
+    placeholder.draw()
+    instruction.draw()
+    #fixation cross 
+    fixation_cross.draw()
+    win.flip()
+    core.wait(0.5)
+    # inter-stimulus
+    placeholder.draw()
+    instruction.draw()
+    win.flip()
+    core.wait(0.5)
+    # stimulus
     placeholder.draw()
     instruction.draw()
     word_stim.draw()
